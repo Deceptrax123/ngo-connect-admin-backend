3bdc498a5b05bdf85478ed3c8db906c238ab829b
Srinitish Srinivasan <srinitishsri@gmail.com>
2022-10-24 11:52:52 +0530

    connect db

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express=require("express");
+const mongoose=require("mongoose");
+
 
 const app=express();
+
+require("./config/db")(mongoose);
 
 app.listen(3000,function(){
     console.log("Server running on port 3000");
